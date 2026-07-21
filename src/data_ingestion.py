5bbb3e57c9c4cf4da1c11cf49c0e060d7e4d7957
Bhavesh <staar1169@gmail.com>
2026-07-21 08:01:50 +0530

    pipeline ready with all components

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -9,15 +9,15 @@ log_dir = 'logs'
 os.makedirs(log_dir,exist_ok=True)
 
 # logging configuration
-logger = logging.getlogger('data_ingestion')
-logger.setlevel('DEBUG')
+logger = logging.getLogger('data_ingestion')
+logger.setLevel('DEBUG')
 
 console_handler = logging.StreamHandler()
-console_handler.setlevel('DEBUG')
+console_handler.setLevel('DEBUG')
 
 log_file_path = os.path.join(log_dir, 'data_ingestion.log')
 file_handler = logging.FileHandler(log_file_path)
-file_handler.setlevel('DEBUG')
+file_handler.setLevel('DEBUG')
  
 formatter = logging.Formatter('%(asctime)s  - %(name)s - %(levelname)s - %(message)s')
 console_handler.setFormatter(formatter)
@@ -45,9 +45,10 @@ def load_data(data_url: str) -> pd.DataFrame:
 def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
     """Preprocess the data"""
     try:
-        df.drop(columns = ['unnamed: 2','unnamed: 3','unnamed: 4'],inplace= True)
+        df.drop(columns = ['Unnamed: 2','Unnamed: 3','Unnamed: 4'],inplace= True)
         df.rename(columns= {'v1':'target','v2':'text'},inplace= True)
         logger.debug('Data preprocessing completed')
+        return df
 
     except KeyError as e:
         logger.error('Missing column in the dataFrame: %s',e)
@@ -74,7 +75,7 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path : str
 def main():
     try:
         test_size = 0.2
-        data_path = ""
+        data_path = "https://raw.githubusercontent.com/STAAR13/MLOPS-Complete-Pipeline/refs/heads/main/experiments/spam.csv"
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df,test_size=test_size,random_state=2)
